b4b1fbd3812ee805be5c61c60f83577b7ea2ca38
Sergei Litovchenko <serglit72@gmail.com>
2018-12-25 19:44:54 -0800

    Added CHANNELS and Login buttons

---
+++ Smack/View/GradientView.swift
@@ -0,0 +1,31 @@
+//
+//  GradientView.swift
+//  Smack
+//
+//  Created by Sergei Litovchenko on 12/25/18.
+//  Copyright © 2018 Sergei Litovchenko. All rights reserved.
+//
+
+import UIKit
+@IBDesignable
+class GradientView: UIView {
+
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    @IBInspectable var bottomColor: UIColor = #colorLiteral(red: 0.1725490196, green: 0.831372549, blue: 0.8470588235, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
