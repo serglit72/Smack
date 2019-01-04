bbcb87a3a64ad9e2cce5976331b30b1df1a0a134
Sergei Litovchenko <serglit72@gmail.com>
2019-01-03 22:08:18 -0800

    Added Smack.xcworkspace/xcuserdata/slitovchenko.xcuserdatad/xcdebugger/

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,7 +8,17 @@
 
 import Foundation
 
+typealias CompleteionHandler = (_ Success : Bool) -> ()
+
+//URL Constants
+let BASE_URL = "https://smackchat2019.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
 //Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
-let UNWIND = "unWindToChannel"
+let UNWIND = "unwindToChannel"
+
+//USER Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
